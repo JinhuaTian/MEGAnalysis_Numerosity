438e0027bbf1f2265eee7818a302aff6a1292da7
JinhuaTian <38090292+JinhuaTian@users.noreply.github.com>
2021-10-22 12:31:42 +0800

    plot_rsa

--- postProcessing/numerosity_source_plotRSA.py
+++ postProcessing/numerosity_source_plotRSA.py
@@ -11,7 +11,7 @@ rootDir = r'E:\temp'
 subjid = 'subj004'
 RDMname = ['FieldArea','Numerosity','ItemArea','Shape']
 subjects_dir = pj(rootDir,'surf')
-for i in [0]: #range(len(RDMname))
+for i in [3]: #range(len(RDMname))
     rsaName = pj(rootDir,subjid,'rsa_run1'+RDMname[i]+'.stc')
     rsa_vals = joblib.load(rsaName)
 
@@ -22,17 +22,16 @@ for i in [0]: #range(len(RDMname))
     brain = rsa_vals.plot(subjid, subjects_dir=subjects_dir, hemi='both', initial_time=peak_time,time_viewer=True)
     #rsa_vals.add_annotation('HCPMMP1_combined', borders=2, subjects_dir=subjects_dir)
 
-brain = rsa_vals.plot(subjects_dir=subjects_dir,hemi='both', initial_time=0,
-                 clim=dict(kind='value', lims=[3, 6, 9]),time_viewer=True)
-movieFname = pj(rootDir,'my.mp4')
-brain.save_movie(time_dilation=20, tmin=0, tmax=0.6,
-                 interpolation='linear', framerate=10)
+# brain = rsa_vals.plot(subjects_dir=subjects_dir, hemi='both', initial_time=0, clim=dict(kind='value', lims=[3, 6, 9]),time_viewer=True)
+# movieFname = pj(rootDir,'my.mp4')
+# brain.save_movie(time_dilation=20, tmin=0, tmax=0.6,interpolation='linear', framerate=10)
 print('All Done')
-'''
+
 # plot the max vertex
 plt.figure()
 plt.plot(rsa_vals.times, rsa_vals.data[peak_vertex])
 plt.xlabel('Time (s)')
 plt.ylabel('Kendall-Tau (alpha)')
 plt.title(f'RSA values at vert {peak_vertex}')
-'''
+plt.show()
+
