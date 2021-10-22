d3dba6e8c5b4e77ae0468af4289fb4caf051000c
JinhuaTian <38090292+JinhuaTian@users.noreply.github.com>
2021-10-22 12:42:35 +0800

    Update numerosity_source_plotRSA.py

--- postProcessing/numerosity_source_plotRSA.py
+++ postProcessing/numerosity_source_plotRSA.py
@@ -11,7 +11,7 @@ rootDir = r'E:\temp'
 subjid = 'subj004'
 RDMname = ['FieldArea','Numerosity','ItemArea','Shape']
 subjects_dir = pj(rootDir,'surf')
-for i in [3]: #range(len(RDMname))
+for i in [0]: #range(len(RDMname))
     rsaName = pj(rootDir,subjid,'rsa_run1'+RDMname[i]+'.stc')
     rsa_vals = joblib.load(rsaName)
 
@@ -23,8 +23,8 @@ for i in [3]: #range(len(RDMname))
     #rsa_vals.add_annotation('HCPMMP1_combined', borders=2, subjects_dir=subjects_dir)
 
 # brain = rsa_vals.plot(subjects_dir=subjects_dir, hemi='both', initial_time=0, clim=dict(kind='value', lims=[3, 6, 9]),time_viewer=True)
-# movieFname = pj(rootDir,'my.mp4')
-# brain.save_movie(time_dilation=20, tmin=0, tmax=0.6,interpolation='linear', framerate=10)
+movieFname = pj(rootDir,'my.mp4')
+brain.save_movie(movieFname, tmin=0, tmax=0.6, interpolation='linear',time_dilation=20, framerate=10, time_viewer=True)
 print('All Done')
 
 # plot the max vertex
