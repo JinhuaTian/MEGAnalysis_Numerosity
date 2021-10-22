52416e98771a70d96fced55efaed7c764333f96a
JinhuaTian <38090292+JinhuaTian@users.noreply.github.com>
2021-10-22 13:08:54 +0800

    Update numerosity_source_plotRSA.py

--- postProcessing/numerosity_source_plotRSA.py
+++ postProcessing/numerosity_source_plotRSA.py
@@ -11,6 +11,8 @@ rootDir = r'E:\temp'
 subjid = 'subj004'
 RDMname = ['FieldArea','Numerosity','ItemArea','Shape']
 subjects_dir = pj(rootDir,'surf')
+
+
 for i in [0]: #range(len(RDMname))
     rsaName = pj(rootDir,subjid,'rsa_run1'+RDMname[i]+'.stc')
     rsa_vals = joblib.load(rsaName)
@@ -18,15 +20,24 @@ for i in [0]: #range(len(RDMname))
     # Find the searchlight patch with highest RSA score
     peak_vertex, peak_time = rsa_vals.get_peak(vert_as_index=True)
 
+    '''
+    surfer_kwargs = dict(
+    hemi='rh', subjects_dir=subjects_dir,
+    clim=dict(kind='value', lims=[0, 0.01, 0.05]), views='lateral',
+    initial_time=peak_time, time_unit='s', size=(800, 800), smoothing_steps=10)
+    brain = rsa_vals.plot(**surfer_kwargs)
+    '''
+
     # Plot the result at the timepoint where the maximum RSA value occurs.
-    brain = rsa_vals.plot(subjid, subjects_dir=subjects_dir, hemi='both', initial_time=peak_time,time_viewer=True)
+    brain = rsa_vals.plot(subjid, subjects_dir=subjects_dir, hemi='both', initial_time=peak_time,time_viewer=True) #initial_time=peak_time
     #rsa_vals.add_annotation('HCPMMP1_combined', borders=2, subjects_dir=subjects_dir)
 
+'''
 # brain = rsa_vals.plot(subjects_dir=subjects_dir, hemi='both', initial_time=0, clim=dict(kind='value', lims=[3, 6, 9]),time_viewer=True)
 movieFname = pj(rootDir,'my.mp4')
-brain.save_movie(movieFname, tmin=0, tmax=0.6, interpolation='linear',time_dilation=20, framerate=10, time_viewer=True)
+brain.save_movie(filename=movieFname,time_dilation=20, tmin=0, tmax=0.6,framerate=24, interpolation='linear',time_viewer=True)
 print('All Done')
-
+'''
 # plot the max vertex
 plt.figure()
 plt.plot(rsa_vals.times, rsa_vals.data[peak_vertex])
